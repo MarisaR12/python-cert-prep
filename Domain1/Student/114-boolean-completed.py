b797a017d1acbf4b854aeb6b749c89fb2fcba981
M.Ramautar <144722341+MarisaR12@users.noreply.github.com>
2025-05-13 12:06:05 -0400

    Update and rename 114-boolean.py to 114-boolean-completed.py

--- Domain1/Student/114-boolean-completed.py
+++ Domain1/Student/114-boolean-completed.py
@@ -1,8 +1,6 @@
-char_life = true
+char_life = True
 score = 0
 if char_life == False:
     print ("game over")
 else:
-    print ("you are still in the game")    
-
-
+    print ("you are still in the game")
